7de8411b63055ff90710e627b907b6daa5abdd7c
Serhan Elmali <serhanelmali@gmail.com>
2024-06-26 03:29:28 +0300

    feat: add windows 98 themed landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,205 @@
-import { SocialMediaLinks } from "@/components/socialMediaLinks";
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center gap-[72px] p-12">
-      <Image src={"/logo.svg"} width={200} height={100} alt="logo" />
-      <iframe
-        width="640"
-        height="315"
-        src="https://www.youtube.com/embed/j65859QXT24?si=rSazjuEnOIda6LS2?autoplay=1"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      />
-      <div className="relative z-10 max-w-[860px]">
-        <p className="text-gray-800 dark:text-white text-center">
-          2018'den bu yana ara verdiğimiz SAMP platformunda, GTA V'deki oyuncu
-          arkadaşlarımızın teşvikleriyle yeniden aktifiz. Vinewood yöneticileri
-          olarak, sizlere yalnızca bir oyuncu kitlesi değil, gerçek bir topluluk
-          sunuyoruz. Açık sözlülük, samimiyet ve anlayışla Vinewood'u birlikte
-          geliştirip yenilikler ekleyerek daha ileriye taşıyacağız.
-        </p>
-      </div>
-      <SocialMediaLinks />
-    </main>
+import Image from "next/image";
+import { useState, useEffect, useCallback } from "react";
+import TypewriterComponent from "typewriter-effect";
+
+const App = () => {
+  const [showTypewriter, setShowTypewriter] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [waitingForInput, setWaitingForInput] = useState(false);
+  const [lineFinished, setLineFinished] = useState(false);
+
+  const lines = [
+    "Merhaba Vinewood Topluluğu!",
+    "Uzun bir aradan sonra tekrar sizlerle buluşmanın mutluluğunu yaşıyoruz.",
+    "2018'den bu yana hem topluluk hem de yöneticiler olarak bir süre ara vermiştik ve SAMP platformu ile birlikte Vinewood'un kapılarını kapatmıştık.",
+    "Bugün, içinde bulunduğumuz şartlar ve GTA:V platformunda oynayan arkadaşlarımızın söylemleri ve deneyimleri, bizi tekrar bu platformda aktif olmaya teşvik etti.",
+    "Vinewood topluluk yöneticileri olarak, sizlere sadece bir oyuncu kitlesi olmanın ötesinde, gerçek bir topluluk olma fırsatı sunuyoruz.",
+    "Açık sözlülük, samimiyet ve anlayış mottosuyla ilerleyen yöneticiler olarak, tekrar aktif olmayı ve sokakları doldurmayı sabırsızlıkla bekliyoruz.",
+    "Bu süreçte temel amacımız, sizin fikir ve düşüncelerinizle birlikte, Vinewood'u ve topluluğumuzu sürekli geliştirerek ve yenilikler ekleyerek yükselen bir değer haline getirmektir.",
+    "Hep birlikte çalışarak, topluluğumuzu daha da ileriye taşıyacağız.",
+    "Sevgilerle.",
+  ];
+
+  const handleClick = () => {
+    setShowTypewriter(true);
+  };
+
+  const handleInput = useCallback(
+    (event) => {
+      if (waitingForInput && lineFinished) {
+        setWaitingForInput(false);
+        setLineFinished(false);
+        setCurrentStep((prevStep) => prevStep + 1);
+      }
+    },
+    [waitingForInput, lineFinished]
   );
-}
+
+  useEffect(() => {
+    if (showTypewriter && currentStep < lines.length) {
+      window.addEventListener("keydown", handleInput);
+      window.addEventListener("click", handleInput);
+    } else {
+      window.removeEventListener("keydown", handleInput);
+      window.removeEventListener("click", handleInput);
+    }
+  }, [showTypewriter, currentStep, handleInput]);
+
+  const waitForInput = () => {
+    return new Promise((resolve) => {
+      const handler = () => {
+        window.removeEventListener("keydown", handler);
+        window.removeEventListener("click", handler);
+        resolve();
+      };
+      window.addEventListener("keydown", handler);
+      window.addEventListener("click", handler);
+      setWaitingForInput(true);
+    });
+  };
+
+  const handleTyping = (typewriter, line, startTime) => {
+    return new Promise((resolve) => {
+      typewriter
+        .changeDelay(20)
+        .typeString(line + "\n\n")
+        .callFunction(() => {
+          setLineFinished(true);
+          resolve();
+        })
+        .start();
+    });
+  };
+  const d = new Date();
+  const hours = d.getHours();
+  const minutes = d.getMinutes();
+
+  return (
+    <div className="overflow-hidden whitespace-pre-wrap flex justify-center font-sans-serif">
+      <div className="fixed bottom-[2%] right-[1%]">
+        <svg
+          width="338"
+          height="75"
+          viewBox="0 0 338 75"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[80px]"
+        >
+          <path
+            d="M43.3687 9.55908L31.2896 74.541H12.7168L0.683105 9.55908H14.5332L21.9805 52.9712L29.4277 9.55908H43.3687ZM59.7163 66.2764H45.7754V1.1582H59.7163V66.2764ZM104.4 71.771H90.459L81.1499 45.8418V71.771H67.209V6.74365H81.1499L90.459 32.8091V6.74365H104.4V71.771ZM144.452 72.6792H111.893V7.69727H144.452V21.6382H125.833V33.2632H144.452V47.2041H125.833V58.8291H144.452V72.6792ZM208.208 8.60547L196.128 73.6328H181.234L177.51 52.063L173.787 73.6328H158.938L146.904 8.60547H160.754L168.201 44.6157L173.787 8.60547H181.234L186.819 44.6157L194.267 8.60547H208.208ZM233.773 51.3818V14.1909H224.464V51.3818H233.773ZM247.714 56.0591L238.405 65.3682H219.833L210.523 56.0591V9.55908L219.833 0.25H238.405L247.714 9.55908V56.0591ZM278.457 51.3818V14.1909H269.148V51.3818H278.457ZM292.398 56.0591L283.089 65.3682H264.516L255.207 56.0591V9.55908L264.516 0.25H283.089L292.398 9.55908V56.0591ZM323.141 56.9673V19.7764H313.832V56.9673H323.141ZM337.082 61.6445L327.772 70.8174H299.891V5.83545H327.772L337.082 15.1445V61.6445Z"
+            fill="white"
+          />
+        </svg>
+      </div>
+      <div className="relative bottom-0 w-full h-screen flex justify-center items-center gap-8 overflow-hidden">
+        <button
+          onClick={handleClick}
+          className="cursor-pointer text-white flex flex-col items-center justify-center font-sans-serif text-[12px] gap-2"
+        >
+          <Image
+            src={"/notepad.png"}
+            width={100}
+            height={100}
+            alt="notepad"
+            className="h-[40px] w-[40px]"
+          />
+          <span>Vinewood GTA5</span>
+        </button>
+        <button
+          onClick={() => window.open("https://discord.gg/vinewood-v", "_blank")}
+          className="text-white flex flex-col items-center justify-center font-sans-serif text-[12px] gap-2 cursor-pointer"
+        >
+          <Image
+            src={"/discord.png"}
+            width={100}
+            height={100}
+            alt="notepad"
+            className="h-[40px] w-[40px]"
+          />
+          <span>Discord</span>
+        </button>
+      </div>
+      {showTypewriter && (
+        <div className="absolute top-0 left-0 h-full p-8 max-w-[1280px] w-full">
+          <div className="window active">
+            <div className="window-toolbar flex items-center gap-1">
+              <Image
+                src={"/notepad.png"}
+                width={100}
+                height={100}
+                alt="notepad"
+                className="h-[18px] w-[18px]"
+              />
+              <div className="window-title">Vinewood GTA5 - Notepad</div>
+              <div className="window-buttons">
+                <div
+                  className="window-button close cursor-pointer"
+                  onClick={() => setShowTypewriter(false)}
+                />
+                <div className="window-button help"></div>
+              </div>
+            </div>
+            <div className="window-wrapper h-full">
+              <div className="flex gap-3 text-black">
+                <span>
+                  <u>F</u>ile
+                </span>
+                <span>
+                  <u>E</u>dit
+                </span>
+                <span>
+                  <u>S</u>earch
+                </span>
+                <span>
+                  <u>H</u>elp
+                </span>
+              </div>
+
+              <div className="window-content bg-white text-black font-sans-serif mb-2">
+                <TypewriterComponent
+                  onInit={async (typewriter) => {
+                    for (let i = 0; i < lines.length; i++) {
+                      setLineFinished(false);
+                      await handleTyping(typewriter, lines[i], i);
+                      await waitForInput();
+                      typewriter.stop();
+                    }
+                  }}
+                />
+                {waitingForInput && (
+                  <div className="animate-pulse">
+                    Devamını görmek için bir tuşa basın.
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div id="toolbar">
+        <div className="toolbar-start-menu">
+          <button className="start-button text-black">Başla</button>
+        </div>
+        <div className="toolbar-separator"></div>
+
+        <div className="toolbar-left">
+          <button className="toolbar-icon ie"></button>
+          <button className="toolbar-icon outlook"></button>
+          <button className="toolbar-icon desktop"></button>
+          <button className="toolbar-icon channels"></button>
+        </div>
+        <div className="toolbar-right">
+          <div className="time text-black">
+            <span className="hour">{hours}</span>:
+            <span className="minutes">{minutes}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
